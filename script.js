58aea9aa4a095e972bad70912beb81f8cd36efe6
akort <akortysh@yandex.ru>
2022-08-12 00:20:50 +0500

    Reimplement grid creation so it is possible to recreate it now

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ let grid;
 let initialColor = '#DDDDDD';
 
 const setUpGrid = (num)=>{
-    grid = document.querySelector('.grid');
+    let content = document.querySelector('.content');
+    let oldGrid = document.querySelector('.grid');
+    content.removeChild(oldGrid);
+    let newGrid = document.createElement('div');
+    content.appendChild(newGrid);
+    newGrid.classList.add('grid');
     for (let i = 0; i < num; i++){
         for (let j = 0; j < num; j++){
             let element = document.createElement('div');
             element.classList.add('cell');
             element.style.backgroundColor = initialColor;
             element.style.gridArea = `${i + 1} / ${j + 1} / ${i + 2} / ${j + 2}`;
-            grid.appendChild(element);
+            newGrid.appendChild(element);
         }
     }
 }
