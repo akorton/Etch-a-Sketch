86af997ad1f8cb50365cc2a2321d0ffa06fab2a2
akort <akortysh@yandex.ru>
2022-08-12 13:05:07 +0500

    Add hover effect and reset button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let grid;
-let initialColor = '#DDDDDD';
+let initialColor = '#E5E5E5';
+let newColor = '#ffa500';
 
 const setUpGrid = (num)=>{
     let content = document.querySelector('.content');
@@ -14,11 +15,27 @@ const setUpGrid = (num)=>{
             element.classList.add('cell');
             element.style.backgroundColor = initialColor;
             element.style.gridArea = `${i + 1} / ${j + 1} / ${i + 2} / ${j + 2}`;
+            element.addEventListener('mouseover', (e)=>{
+                let curElement = e.target;
+                curElement.style.backgroundColor = newColor;
+            });
             newGrid.appendChild(element);
         }
     }
+    grid = newGrid;
 }
 
 setUpGrid(16);
 
+const resetButton = document.querySelector('.reset');
 
+resetButton.addEventListener('click', (e)=>{
+    console.log(grid);
+    resetGrid();
+});
+
+const resetGrid = ()=>{
+    grid.childNodes.forEach((cell)=>{
+        cell.style.backgroundColor = initialColor;
+    });
+}
